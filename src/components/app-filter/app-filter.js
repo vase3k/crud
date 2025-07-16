a8a34f520d61a11b18bc3a7e15b456d06902549e
Vasiliy Vinogradov <vase3k@gmail.com>
2025-07-16 10:12:40 +0300

    dynamic styles

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -2,20 +2,22 @@ import './app-filter.css';
 
 const AppFilter = props => {
     const buttonsData = [
-        { name: 'all', label: 'Все сотрудники' },
-        { name: 'rise', label: 'Сотрудники на повышение' },
-        { name: 'moreThen1000', label: 'З/П больше 1000$' },
+        { name: 'all', label: 'Все сотрудники', colored: false },
+        { name: 'rise', label: 'Сотрудники на повышение', colored: true },
+        { name: 'moreThen1000', label: 'З/П больше 1000$', colored: false },
     ];
 
-    const buttons = buttonsData.map(({ name, label }) => {
+    const buttons = buttonsData.map(({ name, label, colored }) => {
         const active = props.filter === name;
         const clazz = active ? 'btn-light' : 'btn-outline-light';
+        const style = colored ? { color: 'red' } : null;
         return (
             <button
                 className={`btn ${clazz}`}
                 type="button"
                 key={name}
                 onClick={() => props.onFilterSelect(name)}
+                style={style}
             >
                 {label}
             </button>
